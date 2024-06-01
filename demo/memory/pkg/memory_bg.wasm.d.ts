2e2f1403e9f4c76d4b1be4eae6cd455844bcb093
Qinxiang Yang <me@yqxpro.com>
2024-06-02 00:34:44 +0800

    Slint示例程序

---
+++ demo/memory/pkg/memory_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function slint_mock_elapsed_time(a: number): void;
+export function slint_get_mocked_time(): number;
+export function slint_send_mouse_click(a: number, b: number, c: number): void;
+export function slint_send_keyboard_char(a: number, b: number, c: number): void;
+export function send_keyboard_string_sequence(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h1840e94124f2faaa(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h651f277f85503c14(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h2bbb9f3ab228ebc8(a: number, b: number, c: number, d: number): void;
+export function wasm_bindgen__convert__closures__invoke1__h2b3883692096f3d9(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h6c022df3bc1c6dd0(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__h1bc19633555999ad(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfd483d1b4df1dafb(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
